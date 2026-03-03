8a79814ea03253be28a8cce732e6dc1c1bcc7038
J. Francisco <josefranciscomonterosuarez@gmail.com>
2026-03-03 18:01:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const datos = {
-    inicio: "Bienvenido al hub principal. Aquí se presenta la idea general.",
-    proyectos: "Este proyecto consiste en desarrollar soluciones web dinámicas y modulares.",
-    contacto: "Puedes comunicarte vía email o redes profesionales."
-};
 function mostrarSeccion(id) {
     // Ocultar todas
     document.querySelectorAll(".seccion").forEach(sec => {
@@ -10,11 +5,7 @@ function mostrarSeccion(id) {
     });
 
     // Mostrar la seleccionada
-    const contenedor = document
-    .getElementById(id)
-    .querySelector(".contenido");
-
-contenedor.innerHTML = "<p>" + datos[id] + "</p>";
+    document.getElementById(id).classList.add("activa");
 
     // Cambiar clase del body para cambiar color
     document.body.className = id;
